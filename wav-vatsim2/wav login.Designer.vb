03d1881ff133e68752d58b3833eb949d1a1eaa71
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2022-02-12 11:07:37 +0000

    egqtnhgdt

--- wav-vatsim2/wav login.Designer.vb
+++ wav-vatsim2/wav login.Designer.vb
@@ -22,9 +22,113 @@ Partial Class wav_login
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(wav_login))
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.PasswordLabel = New System.Windows.Forms.Label()
+        Me.UsernameLabel = New System.Windows.Forms.Label()
+        Me.OK = New System.Windows.Forms.Button()
+        Me.Cancel = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(-1, 226)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(442, 27)
+        Me.txtPassword.TabIndex = 7
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(-1, 169)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(442, 27)
+        Me.txtUsername.TabIndex = 5
+        '
+        'PasswordLabel
+        '
+        Me.PasswordLabel.Location = New System.Drawing.Point(96, 200)
+        Me.PasswordLabel.Name = "PasswordLabel"
+        Me.PasswordLabel.Size = New System.Drawing.Size(220, 23)
+        Me.PasswordLabel.TabIndex = 6
+        Me.PasswordLabel.Text = "&Password"
+        Me.PasswordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'UsernameLabel
+        '
+        Me.UsernameLabel.Location = New System.Drawing.Point(106, 143)
+        Me.UsernameLabel.Name = "UsernameLabel"
+        Me.UsernameLabel.Size = New System.Drawing.Size(220, 23)
+        Me.UsernameLabel.TabIndex = 4
+        Me.UsernameLabel.Text = "&User name"
+        Me.UsernameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'OK
+        '
+        Me.OK.Location = New System.Drawing.Point(71, 280)
+        Me.OK.Name = "OK"
+        Me.OK.Size = New System.Drawing.Size(156, 42)
+        Me.OK.TabIndex = 8
+        Me.OK.Text = "&OK"
+        '
+        'Cancel
+        '
+        Me.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Cancel.Location = New System.Drawing.Point(258, 280)
+        Me.Cancel.Name = "Cancel"
+        Me.Cancel.Size = New System.Drawing.Size(142, 42)
+        Me.Cancel.TabIndex = 9
+        Me.Cancel.Text = "&Cancel"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(141, -14)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(136, 93)
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(-1, 82)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(455, 20)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "if you have wav Account please enter your username and password "
+        '
+        'wav_login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(474, 341)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Cancel)
+        Me.Controls.Add(Me.OK)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.PasswordLabel)
+        Me.Controls.Add(Me.UsernameLabel)
+        Me.Name = "wav_login"
         Me.Text = "wav_login"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents PasswordLabel As Label
+    Friend WithEvents UsernameLabel As Label
+    Friend WithEvents OK As Button
+    Friend WithEvents Cancel As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
 End Class
--- wav-vatsim2/wav login.vb
+++ wav-vatsim2/wav login.vb
@@ -1,3 +1,50 @@
 ﻿Public Class wav_login
+    Private Sub OK_Click(sender As Object, e As EventArgs) Handles OK.Click
+        myConnection.Close()
+        provider = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source ="
+        'Change the following to your access database location
+        dataFile = "C:\VisStudioProj\wav web\willairwayvirtual34\willairwayvirtual34\app_data\willairwayvirtualDBv1.accdb"
+        connString = provider & dataFile
+        myConnection.ConnectionString = connString
+        myConnection.Open()
+        'the query:
+        Dim cmd As OleDbCommand = New OleDbCommand("Select * FROM [tblaccessinfo] WHERE [Uname] = '" & txtUsername.Text & "' AND [Pword] = '" & txtPassword.Text & "'", myConnection)
+        Dim dr As OleDbDataReader = cmd.ExecuteReader
+        ' the following variable is hold true if user is found, and false if user is not found 
+        Dim userFound As Boolean = False
+        ' the following variables will hold the user first and last name if found.
+        Dim FirstName As String = ""
+        Dim LastName As String = ""
+        Dim Status As String = ""
+        Dim totalHours As String = ""
+        Dim lastflight As String = ""
+
+        'if found:
+        While dr.Read
+            userFound = True
+            FirstName = dr("FName").ToString
+            LastName = dr("LName").ToString
+            Status = dr("status").ToString
+            totalHours = dr("TotalHours").ToString
+            lastflight = dr("lastflight").ToString
+
+
+        End While
+
+        'checking the result
+        If userFound = True Then
+
+            Dim obj As New Form3mll
+
+            obj.Show()
+
+
+            obj.Show()
+            Me.Close()
+        Else
+            MsgBox("YOU DONT EXIST", MsgBoxStyle.OkOnly, "YOU DONT EXIST")
+        End If
+        myConnection.Close()
+    End Sub
 
 End Class
